ad13b56264128063010633295754fb751490800f
José Enrique Aguirre Chavez <jotae_11@hotmail.com>
2022-04-14 01:37:50 -0500

    Handle console args

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tranzact_ProgrammingChallenge_JEACH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No terms were specified for the Search Fight. Please execute again with the search terms.");
+                return;
+            }
+
+            Console.WriteLine("Let's start a Search Fight....");
+            foreach (string arg in args) { 
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
